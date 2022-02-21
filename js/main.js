7827080fd49bc564072d23be4cc8ccab4f9c81a5
Mykola Pererbenesiuk <sanistray@gmail.com>
2022-02-22 00:57:41 +0200

    release

--- js/main.js
+++ js/main.js
@@ -2,31 +2,34 @@
 
 
 $('.auth__button').click(function() {
-    if ($('#username').val().length != 0 && $('#password').val().length != 0) {
-        var userEmail = $('#username').val();
-        var userPassword = $('#password').val()
+    /*    if ($('#username').val().length != 0 && $('#password').val().length != 0) {
+            var userEmail = $('#username').val();
+            var userPassword = $('#password').val()
 
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
 
-        var raw = JSON.stringify({
-            "email": "153@mail.com",
-            "password": "14"
-        });
+            var raw = JSON.stringify({
+                "email": "153@mail.com",
+                "password": "14"
+            });
 
-        var requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: raw,
-            redirect: 'follow'
-        };
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
 
-        fetch("http://192.99.10.113:9001/auth", requestOptions)
-            .then(response => response.text())
-            .then(result => console.log(result))
-            .catch(error => console.log('error', error));
+            fetch("http://192.99.10.113:9001/auth", requestOptions)
+                .then(response => response.text())
+                .then(result => console.log(result))
+                .catch(error => console.log('error', error));
 
-    } else {
-        alert('Enter data for all fields!')
-    }
+        } else {
+            alert('Enter data for all fields!')
+        }
+        */
+
+    $('.crypto__auth').hide(500)
 })
