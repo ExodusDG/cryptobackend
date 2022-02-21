a3dab387f64a89712ffcd2edd8802c51fd816c5a
Mykola Pererbenesiuk <sanistray@gmail.com>
2022-02-22 01:38:17 +0200

    1

--- js/main.js
+++ js/main.js
@@ -2,34 +2,36 @@
 
 
 $('.auth__button').click(function() {
-    /*    if ($('#username').val().length != 0 && $('#password').val().length != 0) {
-            var userEmail = $('#username').val();
-            var userPassword = $('#password').val()
+    if ($('#username').val().length != 0 && $('#password').val().length != 0) {
+        var userEmail = $('#username').val();
+        var userPassword = $('#password').val()
 
-            var myHeaders = new Headers();
-            myHeaders.append("Content-Type", "application/json");
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
 
-            var raw = JSON.stringify({
-                "email": "153@mail.com",
-                "password": "14"
+        const body = `{ "email": "1@mail.com", "password": "1" }`;
+
+        const init = {
+            method: 'POST',
+            headers,
+            body
+        };
+
+        fetch('http://192.99.10.113:9001/auth', init)
+            .then((response) => {
+                return response.json(); // or .text() or .blob() ...
+            })
+            .then((text) => {
+                // text is the response body
+            })
+            .catch((e) => {
+                // error in e.message
             });
 
-            var requestOptions = {
-                method: 'POST',
-                headers: myHeaders,
-                body: raw,
-                redirect: 'follow'
-            };
+    } else {
+        alert('Enter data for all fields!')
+    }
 
-            fetch("http://192.99.10.113:9001/auth", requestOptions)
-                .then(response => response.text())
-                .then(result => console.log(result))
-                .catch(error => console.log('error', error));
 
-        } else {
-            alert('Enter data for all fields!')
-        }
-        */
-
-    $('.crypto__auth').hide(500)
+    //   $('.crypto__auth').hide(500)
 })
